9eae01ce4ca20ef7c975c6dd1c2176dcf67e9b3b
kylelee-dev <leekyle.dev@gmail.com>
2024-09-09 02:48:26 -0400

    Connect to supabase and fetch notes data

--- app/ui.tsx
+++ app/ui.tsx
@@ -1,24 +1,42 @@
+"use client";
+
 import EmptyNote from "@/components/empty-note";
 import Header from "@/components/header";
 import NewNote from "@/components/new-note";
 import NoteViewer from "@/components/note-viewer";
 import Sidebar from "@/components/sidebar";
+import { Database } from "@/types_db";
+import { supabase } from "@/utils/supabase";
+import { useEffect, useState } from "react";
 
 export default function UI() {
-  const notes = [
-    { id: 1, title: "test" },
-    { id: 2, title: "test note 2" },
-  ];
+
+  const [notes, setNotes] = useState<
+    Database["public"]["Tables"]["note"]["Row"][]
+  >([]);
+  const fetchNotes = async () => {
+    const { data, error } = await supabase.from("note").select("*");
+
+    if (error) {
+        alert(error.message);
+        return;
+    } 
+    setNotes(data);
+  };
   const activeNoteId = null;
-  
+  const isCreating = null;
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   return (
     <main className="w-full h-screen flex flex-col">
       <Header />
       <div className="grow relative">
-        <Sidebar notes={notes} />
-        {/* <EmptyNote /> */}
-        {/* <NewNote /> */}
-        <NoteViewer note={notes.find((note) => note.id === activeNoteId)} />
+      <Sidebar notes={notes} />
+        {isCreating ? <NewNote /> : 
+            activeNoteId ? <NoteViewer note={notes.find((note) => note.id === activeNoteId)}/> : <EmptyNote /> 
+        }
       </div>
     </main>
   );
