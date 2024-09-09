30a8c12eaec954af9a0d32740713f9d8836edee5
kylelee-dev <leekyle.dev@gmail.com>
2024-09-09 14:09:52 -0400

    Add note-viewer interactions <Edit/Delete>

--- components/note-viewer.tsx
+++ components/note-viewer.tsx
@@ -1,13 +1,45 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import { supabase } from "@/utils/supabase";
+import { useEffect, useState } from "react";
 
-export default function NoteViewer({note}) {
-  const isEditing = false;
+export default function NoteViewer({ note, setActiveNoteId, fetchNotes }) {
+  const [isEditing, setIsEditing] = useState(false);
   const [title, setTitle] = useState(note?.title);
   const [content, setContent] = useState(note?.content);
 
-  const onSave = () => {}
+  useEffect(() => {
+    setTitle(note.title);
+    setContent(note.content);
+    setIsEditing(false);
+  }, [note]);
+
+  const onSave = async () => {
+    const { data, error } = await supabase
+      .from("note")
+      .update({ title, content })
+      .eq("id", note.id);
+
+    if (error) {
+      alert(error.message);
+    }
+    fetchNotes();
+    setIsEditing(false);
+  };
+  const onDelete = async () => {
+    const { data, error } = await supabase
+      .from("note")
+      .delete()
+      .eq("id", note.id);
+    if (error) {
+      alert(error.message);
+    }
+
+    setActiveNoteId(null);
+    fetchNotes();
+    setIsEditing(false);
+  };
+
   return (
     <div className="w-2/3 p-2 flex flex-col font-bold text-xl gap-2 absolute items-center top-0 right-0 bottom-0">
       {isEditing ? (
@@ -31,13 +63,32 @@ export default function NoteViewer({note}) {
             >
               Save
             </button>
+            <button
+              className=" py-1 px-3 border-2 rounded-full border-green-600 hover:bg-green-200 transition-all duration-300 ease-in-out"
+              onClick={() => onDelete()}
+            >
+              Delete
+            </button>
           </div>
         </>
       ) : (
-        <div className="w-1/2 mt-10 grow">
-          <h1 className="p-2 border-b border-gray-200 ">hhas{title}</h1>
-          <p className="p-2 border mt-10 border-gray-200 h-1/2 grow">asdf{content}</p>
-        </div>
+        <>
+          <h1 className="w-full md:w-1/2 p-2 border-b border-gray-200 ">
+            {title}
+          </h1>
+          <p className="w-full md:w-1/2 p-2 border mt-10 border-gray-200 grow">
+            {content}
+          </p>
+
+          <div className="w-full md:w-1/2 flex justify-end">
+            <button
+              className=" py-1 px-3 border-2 rounded-full border-green-600 hover:bg-green-200 transition-all duration-300 ease-in-out"
+              onClick={() => setIsEditing(true)}
+            >
+              Edit
+            </button>
+          </div>
+        </>
       )}
     </div>
   );
